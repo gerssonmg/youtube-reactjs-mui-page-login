604d58b4c90d38af9779ac4e8e53904d048c63a6
gerson aguiar <gersoncafilho@gmail.com>
2024-06-08 12:10:26 -0300

    youtube - video - env para deploy funcionar no gha e firebase/versel

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isGithubActions = process.env.GITHUB_ACTIONS === true
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/youtube-reactjs-mui-page-login/"
+  base: isGithubActions ? "/youtube-reactjs-mui-page-login/" : "/"
 })
