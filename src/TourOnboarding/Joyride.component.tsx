95e4ddd23c2d3bd868f973a2941c89f7d1b4ca3c
gerson aguiar <gersoncafilho@gmail.com>
2024-06-19 08:40:27 -0300

    up

--- src/TourOnboarding/Joyride.component.tsx
+++ src/TourOnboarding/Joyride.component.tsx
@@ -1,5 +1,4 @@
-import { Box } from '@mui/material';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Joyride, { Step } from 'react-joyride';
 
 
